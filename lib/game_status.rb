2102bf0842638458179ca54723c6e848f26f80fc
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-13 01:51:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || full?(board) == true || draw?(board) == true
+  if won?(board) || full?(board) || draw?(board)
     return true
   else
     return false
