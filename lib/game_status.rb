8dffd7c6ddd4508f382fb55a1f99c93eed1a47ba
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-13 01:11:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,8 @@ def won?(board)
 
       if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return bin
+      elsif full?(board) == false
+        return false
       else
         return false
     end
