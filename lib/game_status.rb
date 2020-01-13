9a016200e902d9e2782ce7528124103768de8e9c
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-13 01:35:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,10 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 =baord[win_index_3]
 
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+      return wins
+    elsif
+      (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return wins
     else
       return false
